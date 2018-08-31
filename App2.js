1946eafa8e676d6d96f6271472e30b474c850b87
Rafael Gomide Bernades Bernades <gomide@MacBook-Air-de-Rafael.local>
2018-08-30 21:53:22 -0300

    teste commit

--- App2.js
+++ App2.js
@@ -8,7 +8,6 @@ import { Router, Scene } from 'react-native-router-flux';
 import ListaItens from './src/components/produtos/ListaItens';
 import ListaClientes from './src/components/clientes/ListaClientes';
 import ListaAlertas from './src/components/alertas/ListaAlertas';
-
 import Agenda from './src/components/agenda/Agenda';
 
 
